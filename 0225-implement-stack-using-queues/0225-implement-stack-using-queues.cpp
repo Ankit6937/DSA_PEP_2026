f28fa3ca0b4495f0094dad370a86a232fadd8ad5
Ankit <152366892+Ankit6937@users.noreply.github.com>
2026-02-11 10:53:37 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.28%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,36 @@
+class MyStack {
+public:
+    queue<int> q1, q2;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        // Step 1: push x into q2
+        q2.push(x);
+
+        // Step 2: move all elements from q1 to q2
+        while(!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        // Step 3: swap q1 and q2
+        swap(q1, q2);
+    }
+    
+    int pop() {
+        int topElement = q1.front();
+        q1.pop();
+        return topElement;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
